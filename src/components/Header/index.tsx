bc0e0bb4b602061f32773159626a31e35423dd12
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-08-10 20:46:21 -0300

    feat: create visual page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../NewTransactionModal';
+import { Container, HeaderContent, NewTransactionButton } from './styles';
+
+const logoSvg = new URL('../../assets/Logo.svg', import.meta.url).href;
+
+export function Header() {
+  return (
+    <Container>
+      <HeaderContent>
+        <img src={logoSvg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </Container>
+  );
+}
