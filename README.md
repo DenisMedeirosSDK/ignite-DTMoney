2baac784b6154b29d84fc477c7c594273f3c3df5
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-09-26 17:47:47 -0300

    finish DT money

---
+++ README.md
@@ -0,0 +1,25 @@
+# DT Money
+
+É um projeto desenvolvido durante um treinamento de programação da Rocketseat 2022, tem como objetivo o gerenciamento
+de entradas e saidas de dinheiro.
+
+![Home](./docs/Home.png)
+![Home modal](./docs/Home_modal.png)
+
+### Tech
+
+- vite
+- React
+- Typescript
+
+### Executar
+
+```bash
+$ npm install
+
+$ npm run dev:server
+#http://localhost:3000/transactions
+
+$ npm run dev
+#http://localhost:5173
+```
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,7 @@ import { SummaryContainer, SummaryCard } from './styles'
 
 export function Summary() {
   const summary = useSummary()
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -22,13 +23,24 @@ export function Summary() {
         </header>
         <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
-      <SummaryCard variant="green">
-        <header>
-          <span>Total</span>
-          <CurrencyDollar size={32} color="#fff" />
-        </header>
-        <strong>{priceFormatter.format(summary.total)}</strong>
-      </SummaryCard>
+
+      {summary.total < 0 ? (
+        <SummaryCard variant="red">
+          <header>
+            <span>Total</span>
+            <CurrencyDollar size={32} color="#fff" />
+          </header>
+          <strong>{priceFormatter.format(summary.total)}</strong>
+        </SummaryCard>
+      ) : (
+        <SummaryCard variant="green">
+          <header>
+            <span>Total</span>
+            <CurrencyDollar size={32} color="#fff" />
+          </header>
+          <strong>{priceFormatter.format(summary.total)}</strong>
+        </SummaryCard>
+      )}
     </SummaryContainer>
   )
 }
